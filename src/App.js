99a62aea6400b929412b2f613f9e957bf80ccc43
Eric Horodyski <eric@horodyski.me>
2019-09-02 20:20:13 -0400

    Add first effect

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './App.scss';
-
 import './firebase/firebase';
-
-
 import configureStore from './store';
-import { addExpense } from './store/expenses/actions';
 import { sortByAmount } from './store/filters/actions';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
 store.dispatch(sortByAmount());
-
 
 function App() {
   return (
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import expensesReducer from './expenses/reducer';
 import filtersReducer from './filters/reducer';
+import thunk from 'redux-thunk';
 
 export default () => {
   return createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer
-    })
+    combineReducers({ expenses: expensesReducer, filters: filtersReducer }),
+    applyMiddleware(thunk)
   );
 };
